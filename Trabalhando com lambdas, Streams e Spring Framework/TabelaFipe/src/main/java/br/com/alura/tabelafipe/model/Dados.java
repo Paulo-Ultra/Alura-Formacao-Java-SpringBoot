c0746c3e4209f743cc5773dd6f66ad847c4b2a39
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2023-12-06 17:19:28 -0300

    Spring e lambdas

---
+++ Trabalhando com lambdas, Streams e Spring Framework/TabelaFipe/src/main/java/br/com/alura/tabelafipe/model/Dados.java
@@ -0,0 +1,6 @@
+package br.com.alura.tabelafipe.model;
+
+public record Dados(
+        String codigo,
+        String nome) {
+}
---
+++ Trabalhando com lambdas, Streams e Spring Framework/screenmatch/src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
@@ -0,0 +1,16 @@
+package br.com.alura.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosSerie(
+        @JsonAlias("Title")
+        String titulo,
+        @JsonAlias("imdbRating")
+        String avaliacao,
+        @JsonAlias("totalSeasons")
+        Integer totalTemporadas)
+        /*@JsonProperty("imdbVotes")  -- Serializa e desserializa(No caso converte de JSON para objeto Java)
+        String votos) */{
+}
