e9a98918b77aacf4c62ec873f86314982960ca1c
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2023-12-08 17:03:32 -0300

    Primeiras configurações da persistência com Postgres SQL

---
+++ Persistência de dados e consultas com Spring JPA/Screenmatch-com-jpa/src/main/java/br/com/alura/screenmatch/service/ConsultaChatGPT.java
@@ -0,0 +1,23 @@
+package br.com.alura.screenmatch.service;
+
+import com.theokanning.openai.completion.CompletionRequest;
+import com.theokanning.openai.service.OpenAiService;
+import org.springframework.beans.factory.annotation.Value;
+
+public class ConsultaChatGPT {
+    @Value("${OPEN_AI_TOKEN}")
+    private static String senha;
+    public static String obterTraducao(String texto) {
+        OpenAiService service = new OpenAiService(senha);
+
+        CompletionRequest requisicao = CompletionRequest.builder()
+                .model("text-davinci-003")
+                .prompt("traduza para o português o texto: " + texto)
+                .maxTokens(1000)
+                .temperature(0.7)
+                .build();
+
+        var resposta = service.createCompletion(requisicao);
+        return resposta.getChoices().get(0).getText();
+    }
+}
